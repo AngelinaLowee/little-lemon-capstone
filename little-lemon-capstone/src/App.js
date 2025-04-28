5d9a53f3a2f1eafa9450ff7eac747424b7a9f2c4
Angelina Lowe <alowe@ecgws.com>
2025-04-28 10:15:58 -0400

    Initial commit

---
+++ little-lemon-capstone/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Welcome to Little Lemon 🍋</h1>
+      </header>
+      <main>
+        <p>We are a family-owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
