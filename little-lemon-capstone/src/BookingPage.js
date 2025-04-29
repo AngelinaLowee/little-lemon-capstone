0e8af3d88faac22ba8effa1426d443809c0e227e
Angelina Lowe <alowe@ecgws.com>
2025-04-29 14:33:06 -0400

    Add HomePage and BookingPage components and setup routing

---
+++ little-lemon-capstone/src/BookingPage.js
@@ -0,0 +1,9 @@
+function BookingPage(){
+    return (
+        <section className="layout-item">
+        <h2>Reserve a Table</h2>
+        <p>Select your date, time, and number of guests.</p>
+        </section>
+    );
+}
+export default BookingPage;
