3895ba2829ae90d957524d9d4898fef595914df7
Angelina Lowe <alowe@ecgws.com>
2025-04-29 13:21:40 -0400

    Styled layout, tex, nav, and images for Little Lemon app final

---
+++ little-lemon-capstone/src/Main.js
@@ -0,0 +1,15 @@
+function Main(){
+    return(
+        <main className="layout-container">
+            <section className="layout-item">
+                <h2>Reserve a Table</h2>
+            <p>Select your date, time, and Number of guests.</p>
+            </section>
+            <section className="layout-item">
+                <h2>Menu</h2>
+                <p>Check out our weekly specials!</p>
+            </section>
+        </main>
+    );
+}
+export default Main; 
